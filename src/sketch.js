c8fab678f2a2cc9d58e997439e7b06534c1a98f9
kvale17 <kevin.valencia24@gmail.com>
2024-02-27 15:23:42 -0800

    Add cell class to each div

--- src/sketch.js
+++ src/sketch.js
@@ -3,6 +3,7 @@ const container = document.querySelector('#sketch-pad');
 for(let i = 1; i<=(16 ** 2); i++){
     const div = document.createElement('div');
     div.setAttribute('id', 'cell-' + i);  
+    div.setAttribute('class', 'cell'); 
     div.textContent = 'Cell' + i; 
     container.appendChild(div);
 }
