61d2a7f901ce94772aa967e88be934cb278f41d2
kvale17 <kevin.valencia24@gmail.com>
2024-02-27 17:31:31 -0800

    Set hard limit for 100 square units grid

--- src/sketch.js
+++ src/sketch.js
@@ -7,9 +7,11 @@ container.addEventListener("mouseover", (e) => {
 addGrid(16);
 
 function addGrid(squareUnits) {
+    if (squareUnits > 100) squareUnits = 100;
+
     const length = `calc(100% / ${squareUnits})`;
 
-    for (let i = 1; i <= squareUnits ** 2; i++) {
+    for (let i = 1; i <= (squareUnits ** 2); i++) {
         const div = document.createElement("div");
         div.setAttribute("id", "cell-" + i);
         div.setAttribute("class", "cell");
