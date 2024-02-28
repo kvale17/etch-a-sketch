900fbcb821a9deb07473036a28c4f862f909bb3a
kvale17 <kevin.valencia24@gmail.com>
2024-02-27 17:17:45 -0800

    Set cell width and height dynamically based on selected size with function

--- src/sketch.js
+++ src/sketch.js
@@ -1,12 +1,20 @@
-const container = document.querySelector('#sketch-pad');
+const container = document.querySelector("#sketch-pad");
 
-for (let i = 1; i <= (16 ** 2); i++) {
-    const div = document.createElement('div');
-    div.setAttribute('id', 'cell-' + i);
-    div.setAttribute('class', 'cell');
-    container.appendChild(div);
-}
-
-container.addEventListener('mouseover', (e) => {
+container.addEventListener("mouseover", (e) => {
     e.target.style.backgroundColor = "blue";
 });
+
+addGrid(16);
+
+function addGrid(size) {
+    const length = `calc(100% / ${size})`;
+
+    for (let i = 1; i <= size ** 2; i++) {
+        const div = document.createElement("div");
+        div.setAttribute("id", "cell-" + i);
+        div.setAttribute("class", "cell");
+        div.setAttribute("style", `width: ${length}; height: ${length};`);
+
+        container.appendChild(div);
+    }
+}
