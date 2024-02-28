d693bf7bc416ebd7739e964374d62f346ef704c9
kvale17 <kevin.valencia24@gmail.com>
2024-02-27 17:49:13 -0800

    Add funciton to get new grid value which is called by button click

--- src/sketch.js
+++ src/sketch.js
@@ -1,8 +1,11 @@
 const container = document.querySelector("#sketch-pad");
+const gridButton = document.getElementById('set-grid');
 
 container.addEventListener("mouseover", (e) => {
     e.target.style.backgroundColor = "blue";
 });
+
+gridButton.addEventListener("click", promptGrid);
 
 addGrid(16);
 
@@ -19,4 +22,26 @@ function addGrid(squareUnits) {
 
         container.appendChild(div);
     }
+}
+
+function promptGrid() {
+    const squareUnits = prompt("Enter the new grid size in units squared", "0");
+
+    if (squareUnits <= 0) {
+        alert("Please enter a positive number greater than 0");
+
+        return promptGrid();
+    }
+    if (isNaN(squareUnits)) {
+        alert("That's not a number");
+
+        return promptGrid();
+    }
+    if (squareUnits > 100) {
+        alert("The max limit is 100");
+
+        return promptGrid();
+    }
+
+    addGrid(parseInt(squareUnits));
 }
